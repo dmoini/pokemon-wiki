667b30d0ade94e3e8db2574cfc3d664137cbccf7
jennifer-s19 <siaojm@yahoo.com>
2020-02-27 18:47:27 -0800

    Adding dropdown button

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -1,7 +1,16 @@
 import "./SearchHeader.css";
 
 import React from "react";
+import {DropdownButton, Dropdown } from "react-bootstrap";
+
 
 export default function SearchHeader() {
-  return <div>TOOD: create SearchHeader component</div>;
+    return ( 
+    <DropdownButton id="Dropdown" title = "Categories">
+      {/* <Dropown.Item as = "Pokemon"><dive onClick={(e) => this.changeValue(e.target.textContent)}>Pokemon</dive></Dropdown.Item> */}
+      <Dropdown.Item as = "Pokemon">Pokemon</Dropdown.Item>
+      <Dropdown.Item as = "Move">Move</Dropdown.Item>
+      <Dropdown.Item as = "Item">Item</Dropdown.Item>
+    </DropdownButton>
+  );
 }
