ba2f571c400e83bd0d229c484250691e821573ed
jennifer-s19 <siaojm@yahoo.com>
2020-02-27 19:12:21 -0800

    Adding dropdown

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -1,16 +1,17 @@
 import "./SearchHeader.css";
 
-import React from "react";
+import React, { useState } from "react";
 import {DropdownButton, Dropdown } from "react-bootstrap";
 
-
 export default function SearchHeader() {
-    return ( 
-    <DropdownButton id="Dropdown" title = "Categories">
+  const [category, setCategory] = useState("Category");
+
+  return ( 
+    <DropdownButton id="dropdown" title={category}>
       {/* <Dropown.Item as = "Pokemon"><dive onClick={(e) => this.changeValue(e.target.textContent)}>Pokemon</dive></Dropdown.Item> */}
-      <Dropdown.Item as = "Pokemon">Pokemon</Dropdown.Item>
-      <Dropdown.Item as = "Move">Move</Dropdown.Item>
-      <Dropdown.Item as = "Item">Item</Dropdown.Item>
+      <Dropdown.Item onClick={() => {setCategory("Pokemon")}}>Pokemon</Dropdown.Item>
+      <Dropdown.Item onClick={() => {setCategory("Move")}}>Move</Dropdown.Item>
+      <Dropdown.Item onClick={() => {setCategory("Item")}}>Item</Dropdown.Item>
     </DropdownButton>
   );
 }
