e2613d88c1ee4ace1c6b1eb475905818e2f9e008
jennifer-s19 <siaojm@yahoo.com>
2020-03-02 14:05:50 -0800

    updating search header

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -1,17 +1,25 @@
 import "./SearchHeader.css";
 
 import React, { useState } from "react";
-import {DropdownButton, Dropdown } from "react-bootstrap";
+import {Form, Button, DropdownButton, Dropdown, FormControl } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function SearchHeader() {
   const [category, setCategory] = useState("Category");
 
   return ( 
+    <div>
     <DropdownButton id="dropdown" title={category}>
-      {/* <Dropown.Item as = "Pokemon"><dive onClick={(e) => this.changeValue(e.target.textContent)}>Pokemon</dive></Dropdown.Item> */}
       <Dropdown.Item onClick={() => {setCategory("Pokemon")}}>Pokemon</Dropdown.Item>
       <Dropdown.Item onClick={() => {setCategory("Move")}}>Move</Dropdown.Item>
       <Dropdown.Item onClick={() => {setCategory("Item")}}>Item</Dropdown.Item>
-    </DropdownButton>
+    </DropdownButton> 
+    <Navbar>
+      <Form inline> 
+      <FormControl type="text" placeholder="Search" className ="searchbar"/>
+      <Button onClick={() => {setCategory("Submit")}}>Submit</Button>
+      </Form>
+    </Navbar>
+    </div>
   );
 }
