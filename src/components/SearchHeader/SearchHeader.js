b27852b2a9898ec475d945e42145ecdca432a682
Donovan Moini <donovanmoini@gmail.com>
2020-03-02 15:11:38 -0800

    Added background image

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -11,54 +11,58 @@ import {
 } from "react-bootstrap";
 import React, { useState } from "react";
 
+import Background from "../../images/pokemon-background.png";
+
 export default function SearchHeader() {
   const [category, setCategory] = useState("Category");
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <Container>
-      <Row>
-        <Form inline className="searchbar center">
-          <DropdownButton id="dropdown" variant="danger" title={category}>
-            <Dropdown.Item
+    <div id="background-img" style={{ backgroundImage: `url(${Background})` }}>
+      <Container>
+        <Row id="search-bar">
+          <Form inline className="center">
+            <DropdownButton id="dropdown" variant="danger" title={category}>
+              <Dropdown.Item
+                onClick={() => {
+                  setCategory("Pokemon");
+                }}
+              >
+                Pokemon
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setCategory("Move");
+                }}
+              >
+                Move
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setCategory("Item");
+                }}
+              >
+                Item
+              </Dropdown.Item>
+            </DropdownButton>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              onChange={e => {
+                setSearchQuery(e.target.value);
+              }}
+            />
+            <Button
+              variant="danger"
               onClick={() => {
-                setCategory("Pokemon");
+                console.log(`${category}: ${searchQuery}`);
               }}
             >
-              Pokemon
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => {
-                setCategory("Move");
-              }}
-            >
-              Move
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => {
-                setCategory("Item");
-              }}
-            >
-              Item
-            </Dropdown.Item>
-          </DropdownButton>
-          <FormControl
-            type="text"
-            placeholder="Search"
-            onChange={e => {
-              setSearchQuery(e.target.value);
-            }}
-          />
-          <Button
-            variant="danger"
-            onClick={() => {
-              console.log(searchQuery);
-            }}
-          >
-            Submit
-          </Button>
-        </Form>
-      </Row>
-    </Container>
+              Submit
+            </Button>
+          </Form>
+        </Row>
+      </Container>
+    </div>
   );
 }
