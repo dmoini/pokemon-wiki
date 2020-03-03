3897c19eaef7a3be84a96019ea050e204b1fb19f
Elise <elise@Ifuekos-MacBook-Pro-2.local>
2020-03-02 23:08:14 -0800

    Move styling completed

--- src/components/MoveSearchResult/MoveSearchResult.js
+++ src/components/MoveSearchResult/MoveSearchResult.js
@@ -1,5 +1,5 @@
 import "./MoveSearchResult.css";
-
+import Image from "../../images/disk.png"
 
 
 import React from "react";
@@ -7,17 +7,31 @@ import React from "react";
 export default function MoveSearchResult({ data }) {
   return (
 
-
-<body className="body">
-  <div id="header">
-    <img id="pokeImage" src=".../../images/disk"/>
+<section class="grid">
+  <div class="image">
+  <img id="disk-image" src={Image}/>
+  
   </div>
 
-  <div id="basicNfo">
-      This move is called {data.name.charAt(0).toUpperCase() + data.name.slice(1)}. The accuracy is {data.accuracy}.
-       The move has {data.pp} PP. The power of this move is {data.power}
+  <div class="info">
+  This move is called {data.name.charAt(0).toUpperCase() + data.name.slice(1)}. The accuracy is {data.accuracy}. 
+  The move has {data.pp} PP. The power of this move is {data.power}.
+  
+
   </div>
-</body>
+</section>
+
+
+// <body className="body">
+//   <div id="header">
+//     <img id="pokeImage" src=".../../images/disk"/>
+//   </div>
+
+//   <div id="basicNfo">
+//       This move is called {data.name.charAt(0).toUpperCase() + data.name.slice(1)}. The accuracy is {data.accuracy}.
+//        The move has {data.pp} PP. The power of this move is {data.power}
+//   </div>
+// </body>
 
   );
 }
