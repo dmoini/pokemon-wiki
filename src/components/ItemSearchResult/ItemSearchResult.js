97d82f1e467181337f0df6dd8eaac230d435a2e1
Donovan Moini <donovanmoini@gmail.com>
2020-03-03 12:22:52 -0800

    Cleaned up ItemSearchResult component

--- src/components/ItemSearchResult/ItemSearchResult.js
+++ src/components/ItemSearchResult/ItemSearchResult.js
@@ -1,13 +1,16 @@
 import "./ItemSearchResult.css";
 
 import React from "react";
+import { capitalize } from "../../helperFunctions";
 
 export default function ItemSearchResult({ data }) {
   return (
-    <div className="content">
-      <h2>{data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
-      <img id="itemImage" src={data.sprites.default} />
-      <p><b>{data.effect_entries[0].short_effect}</b></p>
+    <div className="content pt-2">
+      <h2>{capitalize(data.name)}</h2>
+      <img id="itemImage" src={data.sprites.default} alt="Item" />
+      <p>
+        <b>{data.effect_entries[0].short_effect}</b>
+      </p>
     </div>
   );
 }
