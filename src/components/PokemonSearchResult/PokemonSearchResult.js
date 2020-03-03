9d7966b88d2b2fbd8e09ebe8bbdf6fa57037dc0d
Donovan Moini <donovanmoini@gmail.com>
2020-03-03 12:20:02 -0800

    Cleaned up PokemonSearchResult component

--- src/components/PokemonSearchResult/PokemonSearchResult.js
+++ src/components/PokemonSearchResult/PokemonSearchResult.js
@@ -1,22 +1,35 @@
 import "./PokemonSearchResult.css";
 
 import React from "react";
+import { capitalize } from "../../helperFunctions";
 
 export default function PokemonSearchResult({ data }) {
   return (
     <div className="body">
       <h2 id="header">
-        {data.name.charAt(0).toUpperCase() + data.name.slice(1)} (#{data.id})
-        <img id="pokeImage" src={data.sprites["front_default"]} alt={data.name} />
+        {capitalize(data.name)} (#{data.id})
+        <img
+          id="pokeImage"
+          src={data.sprites["front_default"]}
+          alt={data.name}
+        />
       </h2>
       <div className="basicNfo">
-        <p><b>Height: </b>{data.height}</p>
-        <p><b>Weight: </b>{data.weight}</p>
+        <p>
+          <b>Height: </b>
+          {data.height}
+        </p>
+        <p>
+          <b>Weight: </b>
+          {data.weight}
+        </p>
       </div>
       <div className="types">
-        <p id="typeHeader"><b>{data.types.length>1 ? "Types: " : "Type: "}</b></p>
-        {data.types.map((type) => (
-          <p key={type.slot}>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</p>
+        <p id="typeHeader">
+          <b>{data.types.length > 1 ? "Types: " : "Type: "}</b>
+        </p>
+        {data.types.map(type => (
+          <p key={type.slot}>{capitalize(type.type.name)}</p>
         ))}
       </div>
     </div>
