665bd6d2f61d27b99edd1185f2696314c193f0c0
Isaiah <anyiminene@gmail.com>
2020-03-02 22:17:49 -0800

    pokemon component

--- src/components/PokemonSearchResult/PokemonSearchResult.js
+++ src/components/PokemonSearchResult/PokemonSearchResult.js
@@ -4,10 +4,22 @@ import React from "react";
 
 export default function PokemonSearchResult({ data }) {
   return (
-    <div>
-      <h2>{data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+    <div className="body">
+      <h2 id="header">
+        <img id="pokeImage" src={data.sprites["front_default"]} alt={data.name}/>
+        {data.name.charAt(0).toUpperCase() + data.name.slice(1)} (#{data.id})
+      </h2>
       
-      <h2>You've reached the Pokemon Search Result page!</h2>
+      <div className="basicNfo">
+        <p><b>Height: </b>{data.height}</p>
+        <p><b>Weight: </b>{data.weight}</p>
+      </div>
+      <div className="types">
+        <p id="typeHeader"><b>{data.types.length>1 ? "Types: " : "Type: "}</b></p>
+        {data.types.map((type) => (
+          <p key={type.slot}>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</p>
+        ))}
+      </div>
     </div>
   );
 }
