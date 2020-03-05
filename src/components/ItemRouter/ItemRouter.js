b8fd698f42dc1fc7321dfbc2f0496f7f310eed7b
Elise <elise@Ifuekos-MacBook-Pro-2.local>
2020-03-04 23:59:08 -0800

    Item formatting page

--- src/components/ItemRouter/ItemRouter.js
+++ src/components/ItemRouter/ItemRouter.js
@@ -1,4 +1,6 @@
 import "./ItemRouter.css";
+import Image from "../../images/item_background.png";
+
 
 import React from "react";
 import { checkInvalidAccessToPage } from "../../helperFunctions";
@@ -6,10 +8,26 @@ import { checkInvalidAccessToPage } from "../../helperFunctions";
 export default function ItemRouter({ data }) {
   checkInvalidAccessToPage(data);
   return (
-    <div>
-      <div>This is a Item Router</div>
+    <body id="background"
+    style={{ backgroundImage: `url(${Image})` }} >
+      
+      <div id="info"
+         >
+        This is the entry for the {data.name}. {data.effect_entries[0].effect} 
+        The cost of this item in stores is {data.cost}.
+        If applicable, the Pokemon who can be found holding this in the wild are: {data.held_by_pokemon}. 
+        If applicable, the power of the move Fling when used with this item is: {data.fling_power}, and the effect is {data.fling_effect}.
+       
+        The Pokemon that holds this item is {data.pokemon}. 
+        The details for the version that this item is held in by the Pokémon are: {data.version_details}.
+
+        The rarity of this item is {data.rarity}.
+        The version that this item is held in by the Pokémon is {data.version}.
+
+
+        </div>
+
       <br />
-      <div>{JSON.stringify(data)}</div>
-    </div>
+    </body>
   );
 }
