f7a2ceb895c871b1af4f2556c11227feaa3d3b46
Donovan Moini <donovanmoini@gmail.com>
2020-03-05 00:39:25 -0800

    Cleaned up code and fixed pokemon.name Object issue

--- src/components/ItemRouter/ItemRouter.js
+++ src/components/ItemRouter/ItemRouter.js
@@ -1,31 +1,29 @@
 import "./ItemRouter.css";
+
+import { capitalize, checkInvalidAccessToPage } from "../../helperFunctions";
+
 import Image from "../../images/item_background.png";
-
-
 import React from "react";
-import { checkInvalidAccessToPage } from "../../helperFunctions";
 
 export default function ItemRouter({ data }) {
   checkInvalidAccessToPage(data);
+
   return (
-    <body id="background"
-    style={{ backgroundImage: `url(${Image})` }} >
-      
-      <div id="info"
-         >
-        This is the entry for the {data.name}. {data.effect_entries[0].effect} 
-        The cost of this item in stores is {data.cost}.
-        If applicable, the Pokemon who can be found holding this in the wild are: {data.held_by_pokemon}. 
-        If applicable, the power of the move Fling when used with this item is: {data.fling_power}, and the effect is {data.fling_effect}.
-       
-        The Pokemon that holds this item is {data.pokemon}. 
-        The details for the version that this item is held in by the Pokémon are: {data.version_details}.
-
-        The rarity of this item is {data.rarity}.
-        The version that this item is held in by the Pokémon is {data.version}.
-
-
-        </div>
+    <body id="background" style={{ backgroundImage: `url(${Image})` }}>
+      <div id="info">
+        This is the entry for the {data.name}. {data.effect_entries[0].effect}
+        The cost of this item in stores is {data.cost}. If applicable, the
+        Pokemon who can be found holding this in the wild are:{" "}
+        {data.held_by_pokemon.map(p => {
+          return <p>{capitalize(p.pokemon.name)}</p>;
+        })}
+        . If applicable, the power of the move Fling when used with this item
+        is: {data.fling_power}, and the effect is {data.fling_effect}. The
+        Pokemon that holds this item is {data.pokemon}. The details for the
+        version that this item is held in by the Pokémon are:{" "}
+        {data.version_details}. The rarity of this item is {data.rarity}. The
+        version that this item is held in by the Pokémon is {data.version}.
+      </div>
 
       <br />
     </body>
