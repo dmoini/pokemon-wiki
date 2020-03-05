8b09c24e1ff7c06a43146c0ed0aebfdd8b1cfe9c
jennifer-s19 <siaojm@yahoo.com>
2020-03-05 01:20:42 -0800

    basic info and image

--- src/components/PokemonRouter/PokemonRouter.js
+++ src/components/PokemonRouter/PokemonRouter.js
@@ -1,15 +1,54 @@
 import "./PokemonRouter.css";
+import HomeHeader from "../HomeHeader/HomeHeader";
 
 import React from "react";
 import { checkInvalidAccessToPage } from "../../helperFunctions";
+import { capitalize } from "../../helperFunctions";
+
 
 export default function PokemonRouter({ data }) {
   checkInvalidAccessToPage(data);
   return (
-    <div>
-      <div>This is a Pokemon Router</div>
-      <br />
-      <div>{JSON.stringify(data)}</div>
+    <div> 
+      <HomeHeader />
+    <div className="bod">
+      <h2 id="head">
+        {capitalize(data.name)} 
+        <br/>
+        (#{data.id}) 
+        <br/>
+       <img
+          id="pImage"
+          src={data.sprites["front_default"]}
+          alt={data.name}
+          style = {{
+            alignContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+            width: 500, 
+          }}
+        />
+      </h2>
+      <div className="Info">
+        <p>
+          <b>Species: </b>
+          {data.name}
+        </p>
+        <p>
+          <b>Order: </b>
+          {data.order}
+        </p>
+        <p>
+          <b>Height: </b>
+          {data.height}
+        </p>
+        <p>
+          <b>Weight: </b>
+          {data.weight}
+        </p>
+      </div>
+
     </div>
+  </div>
   );
 }
